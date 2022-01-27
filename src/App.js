28bfbb8f7323b1b911b96696ce6b497783e494a9
danielon9320 <daniel_ls93@live.com.ar>
2022-01-27 19:45:01 -0300

    css aplicado. api consumida correctamente y creacion de componente Coin

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Axios from "axios";
+import Coin from "./components/Coin";
 
 function App() {
+
+  
+  //allows to have a variable when its values changes it will trigger and re render in our page to show the new value being displayed in the page
+  const [listOfCoins, setListOfCoins] = useState([]);
+
+  //when page renders show info
+  useEffect(() => {
+    Axios.get("https://api.coinstats.app/public/v1/coins?skip=0&limit=10").then(
+      (response) => {
+        setListOfCoins(response.data.coins);
+        //console.log(response.data);
+      }
+    );
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="cryptoHeader"></div>
+      <div className="cryptoDisplay">
+        {listOfCoins.map((coin) => {
+          return (
+            <Coin
+              name={coin.name}
+              icon={coin.icon}
+              price={coin.price}
+              symbol={coin.symbol}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
